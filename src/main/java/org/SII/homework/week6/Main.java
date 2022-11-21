87a36e17bf487cbd121cf0d7c00de5242a5afe98
Emi14 <emiionut@gmail.com>
2022-11-21 13:27:09 +0200

    Adding initial java structure + week 6 homework package

---
+++ src/main/java/org/SII/homework/week6/Main.java
@@ -0,0 +1,7 @@
+package org.SII.homework.week6;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is the main class for week 7 homework");
+    }
+}
