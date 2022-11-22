92b7c169379c5dd63acac5e7cbd800d5ad9353ea
s13fan <118838903+s13fan@users.noreply.github.com>
2022-11-22 20:29:41 +0200

    MiFirstLine

--- src/main/java/org/siit/homework/week6/Main.java
+++ src/main/java/org/siit/homework/week6/Main.java
@@ -2,6 +2,9 @@ package org.siit.homework.week6;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("This is the main class for week 6 homework");
+        System.out.println("Hai noroc soarece");
     }
 }
+
